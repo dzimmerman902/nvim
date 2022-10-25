584ab7d921bb7e8dfd69ad829e595dae2ba915cf
Devin Zimmerman <dzimmerman902@gmail.com>
2022-10-25 15:24:29 -0600

    bash beauty

--- lua/dzimm/null-ls.lua
+++ lua/dzimm/null-ls.lua
@@ -5,8 +5,9 @@ end
 
 null_ls.setup {
     sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.beautysh,
         null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.stylua,
     },
 }
