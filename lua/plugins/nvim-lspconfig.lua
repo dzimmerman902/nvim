6e47db5f7319c493f629da11b96b96bab9c5b52d
Devin Zimmerman <devinzimmerman@Devins-MacBook-Pro.local>
2024-12-05 19:30:06 -0700

    removed comments

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -162,23 +162,15 @@ return {
         --  - settings (table): Override the default settings passed when initializing the server.
         --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
         local servers = {
-            -- Some languages (like typescript) have entire language plugins that can be useful:
-            --    https://github.com/pmizio/typescript-tools.nvim
-            --
-            -- But for many setups, the LSP (`ts_ls`) will work just fine
             ts_ls = {},
             marksman = {},
             eslint = {},
             lua_ls = {
-                -- cmd = {...},
-                -- filetypes = { ...},
-                -- capabilities = {},
                 settings = {
                     Lua = {
                         completion = {
                             callSnippet = 'Replace',
                         },
-                        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
                         diagnostics = { disable = { 'missing-fields' } },
                     },
                 },
