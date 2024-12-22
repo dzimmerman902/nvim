31778836425b5d3fc1f9139a5726fe78d1dc9b99
Devin Zimmerman <dzimmerman@tree.com>
2024-12-22 08:26:43 -0700

    Small updates

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,6 @@
 return {
     'echasnovski/mini.nvim',
     config = function()
-        require('mini.ai').setup({ n_lines = 500 })
         require('mini.cursorword').setup({})
     end,
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -52,20 +52,19 @@ return {
         vim.api.nvim_create_autocmd('User', {
             pattern = 'VeryLazy',
             callback = function()
-        -- stylua: ignore start
-        _G.dd = function(...) Snacks.debug.inspect(...) end
-        _G.bt = function() Snacks.debug.backtrace() end
-        vim.print = _G.dd -- Override print to use snacks for `:=` command
+                -- stylua: ignore start
+                _G.dd = function(...) Snacks.debug.inspect(...) end
+                _G.bt = function() Snacks.debug.backtrace() end
+                vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-        Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-        Snacks.toggle.diagnostics():map("<leader>ud")
-        Snacks.toggle.line_number():map("<leader>ul")
-        Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2, }):map(
-          "<leader>uc")
-        Snacks.toggle.treesitter():map("<leader>uT")
-        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background", }):map("<leader>ub")
+                Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+                Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+                Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+                Snacks.toggle.diagnostics():map("<leader>ud")
+                Snacks.toggle.line_number():map("<leader>ul")
+                Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2, }):map("<leader>uc")
+                Snacks.toggle.treesitter():map("<leader>uT")
+                Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background", }):map("<leader>ub")
                 -- stylua: ignore end
             end,
         })
