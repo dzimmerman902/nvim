8b06822e10b22aadecb4667b6c4ea8be9678757a
Devin Zimmerman <devinzimmerman@Devins-MacBook-Pro.local>
2024-12-22 08:34:11 -0700

    More small updates

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -162,6 +162,7 @@ return {
         --  - settings (table): Override the default settings passed when initializing the server.
         --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
         local servers = {
+            pyright = {},
             ts_ls = {},
             marksman = {},
             eslint = {},
