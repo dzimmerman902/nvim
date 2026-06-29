f7e8cb5da53c3ab5193c92c5dd2bed548c279eb0
Devin Zimmerman <dzimmerman@tree.com>
2026-06-29 15:30:50 -0600

    update to toggle term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,9 +5,24 @@ return {
 		config = function()
 			require("toggleterm").setup({
 				open_mapping = [[<c-\>]],
-				direction = "float",
+				direction = "vertical",
+				size = 60,
 				close_on_exit = true,
 				shell = vim.o.shell,
+			})
+
+			-- Navigate out of terminal into adjacent windows with normal keymaps
+			local function set_terminal_keymaps()
+				local opts = { buffer = 0 }
+				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+				vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+			end
+
+			vim.api.nvim_create_autocmd("TermOpen", {
+				pattern = "term://*toggleterm#*",
+				callback = set_terminal_keymaps,
 			})
 		end,
 	},
