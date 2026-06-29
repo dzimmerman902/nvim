ed0a7656151389ba8003b95a2171d96046a13a1e
Devin Zimmerman <dzimmerman@tree.com>
2026-06-29 14:39:47 -0600

    update to gitsigns

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,7 +1,6 @@
 return {
 	{
 		"kdheepak/lazygit.nvim",
-		lazy = true,
 		cmd = {
 			"LazyGit",
 			"LazyGitConfig",
@@ -9,21 +8,12 @@ return {
 			"LazyGitFilter",
 			"LazyGitFilterCurrentFile",
 		},
-		-- optional for floating window border decoration
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
-		-- setting the keybinding for LazyGit with 'keys' is recommended in
-		-- order to load the plugin when the command is run for the first time
 		keys = {
-				{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-				{ "<leader>gF", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit current file" },
+			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+			{ "<leader>gF", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit current file" },
 		},
-		config = function()
-			-- Configure lazygit to open files in the current nvim instance
-			vim.g.lazygit_floating_window_scaling_factor = 0.9
-			vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
-			vim.g.lazygit_use_neovim_remote = 1 -- Use neovim remote to open files
-		end,
 	},
 }
